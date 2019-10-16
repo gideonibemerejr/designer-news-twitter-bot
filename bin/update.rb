e275c8f4222fccc87172a5752a28bd573efc154c
Gideon Ibemere Jr <gideon.ibemere.jr@gmail.com>
2019-10-16 02:19:57 -0500

    Initial commit

---
+++ bin/update.rb
@@ -0,0 +1,15 @@
+require 'dotenv/load'
+require "twitter"
+require "httparty"
+require "nokogiri"
+
+
+twitter = Twitter::REST::Client.new do |config|
+    config.consumer_key = ENV['CONSUMER_KEY']
+    config.consumer_secret = ENV['CONSUMER_SECRET']
+    config.access_token = ENV['ACCESS_TOKEN']
+    config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
+end
+
+
+
